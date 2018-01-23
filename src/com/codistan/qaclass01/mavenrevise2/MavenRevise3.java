86e16494a7447d06f35ccec5d2287f854374a5dc
serda <serda@DESKTOP-0BU2OA6>
2018-01-22 19:39:45 -0600

    MovenRevise2

---
+++ src/com/codistan/qaclass01/mavenrevise2/MavenRevise3.java
@@ -0,0 +1,25 @@
+package com.codistan.qaclass01.mavenrevise2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class MavenRevise3 {
+	String url=System.setProperty("webdriver.chrome.driver", "C:\\Users\\serda\\OneDrive\\Documents\\Chromedriver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+	@Test
+	public void Ebay() {
+driver.get("https://www.ebay.com");
+		
+		driver.findElement(By.id("gh-ac")).sendKeys("coffe"+Keys.ENTER);
+	}
+	@AfterTest
+	public void endTest() {
+		driver.close();
+	}
+}
+
